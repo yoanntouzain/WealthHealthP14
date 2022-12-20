09db33fc377a848dd7facb45a69b9e7137f56da5
yoanntouzain <ytouzain@gmail.com>
2022-12-20 17:51:57 +0100

    création du build

--- wealthhealth/src/index.jsx
+++ wealthhealth/src/index.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, HashRouter } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import EmployeeList from './pages/Employee-list/Employee-list'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/employee-list" element={<EmployeeList />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 )
